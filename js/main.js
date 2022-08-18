9993f70e97b7f15e3d69c77b65933a613e594802
jeongmin <hyun5929@naver.com>
2022-08-18 11:48:07 +0900

    logo 변경

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 import '../scss/main.scss'
+
+window.addEventListener("DOMContentLoaded", function() {
+
+})
+
+document.querySelector('nav svg').addEventListener('click', () => {
+    
+    const mobGnbEl = document.querySelector('nav .mobGnb')
+    const svgEl = document.querySelector('nav svg')
+    const bodyEl = document.querySelector('body')
+
+    bodyEl.classList.toggle("scrNone");
+    svgEl.classList.toggle("active");
+    mobGnbEl.classList.toggle("active");
+})

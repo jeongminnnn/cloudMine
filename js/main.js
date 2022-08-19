a33da4e308d765532ce7d34de202cc958df892a4
jeongmin <hyun5929@naver.com>
2022-08-19 12:46:15 +0900

    mysql_user 그룹 js 추가

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ import '../scss/main.scss'
 
 window.addEventListener("DOMContentLoaded", function() {
 
+    // mysql_user_증가 함수
+    userGroupFunc();
 })
 
 document.querySelector('nav svg').addEventListener('click', () => {
@@ -14,3 +16,31 @@ document.querySelector('nav svg').addEventListener('click', () => {
     svgEl.classList.toggle("active");
     mobGnbEl.classList.toggle("active");
 })
+
+function userGroupFunc() {
+
+    const mysql_user = document.querySelector('.mysql_user')
+    const userKey = mysql_user.dataset.group
+    const userGroup = {
+        miner: 0.00001,
+        contributor: 0.00002,
+        ambassador: 0.00004,
+        node: 0.001
+    }
+
+    for (let key in userGroup) {
+
+        const keyVal = userGroup[key]
+        let userVal = parseFloat(mysql_user.value)    
+
+        if(key == userKey) {
+
+            setInterval(() => {
+
+                userVal += keyVal;
+
+                mysql_user.value = (Math.floor(userVal * 100000) / 100000).toFixed(5)
+            }, 1000);
+        }
+    }
+}

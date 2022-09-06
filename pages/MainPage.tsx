2339d8c9dbad8ba2fdfaa0f10756969248a9f176
jeongmin <hyun5929@naver.com>
2022-09-06 17:28:10 +0900

    eject 되돌리기

---
+++ pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const MainPage: React.FC = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default MainPage
